729585145e0a91d9f626e610125c99c5c7c3b2db
HashimAli1986 <h.j.ksa@me.com>
2025-05-21 14:14:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ def send_telegram_message(text):
 assets = {
     "ذهب": {"symbol": "GC=F"},
     "بيتكوين": {"symbol": "BTC-USD"},
-    "SPX": {"symbol": "ES=F"},   # Futures for S&P 500
-    "NDX": {"symbol": "NQ=F"}    # Futures for Nasdaq
+    "US500": {"symbol": "^US500"},  # تم التغيير من SPX إلى US500
+    "NDX": {"symbol": "^NDX"}
 }
 
 def fetch_intraday_data(symbol):
